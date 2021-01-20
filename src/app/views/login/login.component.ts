042d1e86861c5261a24b7662e4cf94335e7f384a
Wesley <wesleysb07@gmail.com>
2021-01-20 03:03:33 -0300

    SweetAlert2 - Servico de Alerta

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RequestLogin } from 'src/app/resources/models/RequestLogin';
+import { AlertService } from 'src/app/resources/services/alert.service';
 import { LoginService } from 'src/app/resources/services/login.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
 
   public requestLogin: RequestLogin;
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private alertService: AlertService ) { }
 
   ngOnInit(): void {
     this.requestLogin = new RequestLogin();
@@ -20,10 +21,10 @@ export class LoginComponent implements OnInit {
   public doLogin(): void {
     this.loginService.doLogin(this.requestLogin).subscribe(
       (data) => {
-        console.log(data)
+        this.alertService.info('Funcionalidade ainda não implementada!');
       },
-      (error) => {
-        console.error(error);
+      (httpError) => {
+        this.alertService.error(httpError.error.message);
       }
     );
   }
