aa768fd97b376d69a872787a790bfe62d2b69cb7
Wesley <wesleysb07@gmail.com>
2021-01-22 01:03:16 -0300

    CanActivate - Guarda de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardService } from './resources/services/auth-guard.service';
 import { LoginComponent } from './views/login/login.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   {
-    path:'dashboard',
+    path: 'dashboard',
+    canActivate: [AuthGuardService],
     loadChildren: () => import('./views/dashboard/dashboard.module').then(
       (m) => m.DashboardModule
     ),
-  }
+  },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
